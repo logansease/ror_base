ed2efe989be3174a26aff8eabdf1ea301189686b
Logan Sease <lsease@gmail.com>
2014-05-07 11:10:52 -0400

    moved help page to dynamic

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,6 @@ SampleApp::Application.routes.draw do
   get '/signup', :to => 'users#new'
   get '/contact', :to => 'pages#contact'
   get '/about', :to => 'pages#about'
-  get '/help', :to => 'pages#help'
   get '/signin', :to => 'sessions#new'
   get '/signout', :to => 'sessions#destroy'
   get '/admin', :to => 'pages#admin'
